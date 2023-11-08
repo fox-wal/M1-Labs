f65a8c15dd4723bac93af93f342da8a18e9da4e6
ac1dTigR <artypaws7@gmail.com>
2023-11-08 12:38:38 +0000

    feat(W7/Bank Account): remove redundancies by adding inheritance

--- W7-debugging-and-documentation/2. Bank Account/SavingsAccount.py
+++ W7-debugging-and-documentation/2. Bank Account/SavingsAccount.py
@@ -1,14 +1,12 @@
-class SavingsAccount:
+import Account
+class SavingsAccount(Account):
     """
     This is a class that represents a savings account
     """
     _annual_interest_rate = 4.3
 
     def __init__(self,account_number:int,account_holder:str,account_type:str,opening_balance:float=10):
-        self._number = account_number
-        self._holder = account_holder
-        self._type = account_type
-        self._savings_balance = opening_balance
+        super().__init__(account_number, account_holder, account_type, opening_balance)
 
     def update_interest_rate(self,new_rate:float)->None:
         if new_rate < 0:
@@ -17,10 +15,4 @@ class SavingsAccount:
             self._annual_interest_rate = new_rate
 
     def calculate_monthly_interest(self)->None:
-        self._savings_balance += self._savings_balance*(self._annual_interest_rate/12)
-
-    def get_balance(self):
-        return self._savings_balance
-
-    def __str__(self):
-        return f"Account information:\nHolder: {self._holder}, number: {self._number}, balance: {self._savings_balance}"
+        self._balance += self._balance*(self._annual_interest_rate/12)
